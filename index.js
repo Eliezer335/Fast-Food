0bf0a88f7e4d1ac8c37a4c8c3c2a2acf83b9f3de
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-07-31 21:31:40 -0300

    ✨ Feat: Criando  a função de selecinar os pratos (aindao nao funcionando)

---
+++ index.js
@@ -0,0 +1,15 @@
+const escolheCaixa = (informacao) => {
+    console.log(informacao)
+    const caixa = document.getElementById(informacao.id);
+    //const img = docume...
+    
+    caixa.classList.add("check");
+    //Preciso retirar a seleção dos selecionados e colocar no que foi selecionado pela função;
+    caixa.classList.remove("check");
+};
+
+const escolhePedido = (pedido)=>{
+    const prato = document.getElementById(prato_01.id)
+
+
+}

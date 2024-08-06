c6d941c2b68824a6641e5ce0325b8ead733692b9
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-08-05 21:24:19 -0300

    ✨ Feat: criando as funcoes de selecionar os pedidos

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+let prato = "";
+let precoPrato = "";
+let bebida = "";
+let sobremesa = "";
+
 const escolheCaixa = (informacao) => {
     console.log(informacao)
     const caixa = document.getElementById(informacao.id);
@@ -8,8 +13,57 @@ const escolheCaixa = (informacao) => {
     caixa.classList.remove("check");
 };
 
-const escolhePedido = (pedido)=>{
-    const prato = document.getElementById(prato_01.id)
+const escolhePedido = (informacao)=>{
+    const prato1 = document.querySelectorAll(".prato");
+    console.log(prato1)
+
+    prato1.forEach((prato) => {
+        prato.classList.remove("check");
+    }) //executa a mesma ação para todos os elementos
+
+    const prato = document.getElementById(informacao.id);
+    prato.classList.add("check");
+
+   prato = informacao.nome;
+   precoPrato = informacao.idade
+
+   novaFuncao();
+
+}
+const novaFuncao = () => {
+    console.log(prato, precoPrato)
+};
+
+const escolheBebida = (informacao)=>{
+    const bebida1 = document.querySelectorAll(".prato_1");
+    // console.log(bebida1)
+
+    bebida1.forEach((prato) => {
+        prato.classList.remove("check");
+    }) //executa a mesma ação para todos os elementos
+
+    const prato = document.getElementById(informacao.id);
+    prato.classList.add("check");
+
+   prato = informacao.nome;
+   precoPrato = informacao.idade
+
+
+}
+
+const escolheSobremesa = (informacao)=>{
+    const sobremesa1 = document.querySelectorAll(".prato_2");
+    // console.log(bebida1)
+
+    sobremesa1.forEach((prato) => {
+        prato.classList.remove("check");
+    }) //executa a mesma ação para todos os elementos
+
+    const prato = document.getElementById(informacao.id);
+    prato.classList.add("check");
+
+   prato = informacao.nome;
+   precoPrato = informacao.idade
 
 
 }
